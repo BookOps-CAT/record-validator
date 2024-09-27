efb4b87285ec317800e80cf3fdd5f3d1987cb64d
Charlotte Kostelic <charlottekostelic@gmail.com>
2024-09-27 10:58:20 -0400

    added get_material_type to adapters.py

--- record_validator/adapters.py
+++ record_validator/adapters.py
@@ -1,3 +1,5 @@
+import re
+from collections import defaultdict
 from typing import Annotated, Union
 from pydantic import Tag, Discriminator, TypeAdapter
 from pymarc import Field as MarcField
@@ -19,6 +21,53 @@ from record_validator.field_models import (
     OrderField,
     OtherDataField,
 )
+
+
+def get_material_type(fields: list) -> str:
+    if not isinstance(fields, list) or not all(
+        isinstance(i, (MarcField, dict)) for i in fields
+    ):
+        return "other"
+    multivol_pattern = re.compile(r"^(\d+)( v\.| volumes)( :$| ;$|$| $)")
+    pamphlet_pattern = re.compile(r"^(\d|[0-4][0-9])( p\.| pages)( :$| ;$|$| $)")
+    catalogues_pattern = re.compile(r"^[cC]atalogue(s?) [rR]aisonn[eé](s?)")
+    phys_desc = []
+    for field in fields:
+        tag = field.tag if isinstance(field, MarcField) else list(field.keys())[0]
+        if tag.startswith("00"):
+            continue
+        if isinstance(field, MarcField):
+            subfields = field.subfields_as_dict()
+        else:
+            subfield_dict = defaultdict(list)
+            for s in field[tag].get("subfields"):
+                for k, v in s.items():
+                    subfield_dict[k].append(v)
+
+            subfields = dict(subfield_dict)
+        if "v" in subfields.keys() and any(
+            match is not None
+            for match in [catalogues_pattern.match(i.strip()) for i in subfields["v"]]
+        ):
+            return "other"
+        elif tag == "960" and "t" in subfields.keys() and subfields["t"] == ["PAD"]:
+            return "other"
+        elif tag == "300" and "a" in subfields.keys():
+            phys_desc.extend(subfields["a"])
+        else:
+            continue
+    if any(
+        match is not None
+        for match in [multivol_pattern.match(i.strip()) for i in phys_desc]
+    ):
+        return "other"
+    elif any(
+        match is not None
+        for match in [pamphlet_pattern.match(i.strip()) for i in phys_desc]
+    ):
+        return "other"
+    else:
+        return "monograph"
 
 
 def get_monograph_tag(field: Union[MarcField, dict]) -> str:
