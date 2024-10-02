a0f15c0219b36731cf9c5c850032f34ac6810fd7
Charlotte Kostelic <charlottekostelic@gmail.com>
2024-10-02 11:40:28 -0400

    added functions to adapters.py

--- record_validator/adapters.py
+++ record_validator/adapters.py
@@ -5,6 +5,7 @@ from pydantic import Tag, Discriminator, TypeAdapter
 from pymarc import Field as MarcField
 from record_validator.constants import AllFields
 from record_validator.field_models import (
+    AuxBibCallNo,
     BibCallNo,
     BibVendorCode,
     ControlField001,
@@ -17,10 +18,11 @@ from record_validator.field_models import (
     ItemField,
     LCClass,
     LibraryField,
-    MonographOtherField,
+    MonographDataField,
     OrderField,
     OtherDataField,
 )
+from record_validator.utils import get_subfield_from_code
 
 
 def get_material_type(fields: list) -> str:
@@ -100,7 +102,7 @@ MonographRecordAdapter: TypeAdapter = TypeAdapter(
             Annotated[LCClass, Tag("050")],
             Annotated[LibraryField, Tag("910")],
             Annotated[OrderField, Tag("960")],
-            Annotated[MonographOtherField, Tag("data_field")],
+            Annotated[MonographDataField, Tag("data_field")],
             Annotated[ControlField001, Tag("001")],
             Annotated[ControlField003, Tag("003")],
             Annotated[ControlField005, Tag("005")],
@@ -134,6 +136,7 @@ OtherRecordAdapter: TypeAdapter = TypeAdapter(
 
 FieldAdapter: TypeAdapter = TypeAdapter(
     Union[
+        AuxBibCallNo,
         BibCallNo,
         BibVendorCode,
         ControlField001,
@@ -146,8 +149,170 @@ FieldAdapter: TypeAdapter = TypeAdapter(
         ItemField,
         LCClass,
         LibraryField,
-        MonographOtherField,
+        MonographDataField,
         OrderField,
         OtherDataField,
     ]
 )
+
+
+def get_adapter(fields: list) -> tuple[str, TypeAdapter]:
+    vendor = get_vendor_code(fields)
+    vendor_code = vendor.lower() if vendor is not None else None
+    material_type = get_record_type(fields=fields, vendor_code=vendor_code)
+    match vendor_code, material_type:
+        case "auxam", "other":
+            return f"{vendor_code}_other", TypeAdapter(AuxOtherFields)
+        case vendor_code, "other" if isinstance(vendor_code, str):
+            return f"{vendor_code}_other", TypeAdapter(OtherFields)
+        case vendor_code, "other" if not isinstance(vendor_code, str):
+            return "other", TypeAdapter(OtherFields)
+        case vendor_code, "monograph" if isinstance(vendor_code, str):
+            return f"{vendor_code}_monograph", TypeAdapter(MonographFields)
+        case _:
+            return "monograph", TypeAdapter(MonographFields)
+
+
+def get_record_type(fields: list, vendor_code: Union[str, None]) -> str:
+    if not isinstance(fields, list) or not all(
+        isinstance(i, (MarcField, dict)) for i in fields
+    ):
+        return "other"
+    phys_desc: list = []
+    for field in fields:
+        tag = field.tag if isinstance(field, MarcField) else list(field.keys())[0]
+        if (
+            tag.startswith("6")
+            and pattern_match(get_subfield_from_code(field, tag, "v"), "catalogues")
+            is True
+        ):
+            return "other"
+        elif (
+            tag == "852"
+            and vendor_code in ["AUXAM", "auxam"]
+            and pattern_match(get_subfield_from_code(field, tag, "h"), "aux_call_no")
+            is True
+        ):
+            return "other"
+        elif tag == "960" and get_subfield_from_code(field, tag, "t") == ["PAD"]:
+            return "other"
+        elif tag == "300":
+            phys_desc.extend(get_subfield_from_code(field, tag, "a"))
+        else:
+            continue
+    if pattern_match(phys_desc, "multivol") is True:
+        return "other"
+    elif pattern_match(phys_desc, "pamphlet") is True:
+        return "other"
+    else:
+        return "monograph"
+
+
+def get_vendor_code(fields: list) -> Union[str, None]:
+    if not all(isinstance(i, (MarcField, dict)) for i in fields):
+        return None
+    if all(isinstance(i, MarcField) for i in fields):
+        fields = [i for i in fields if i.tag == "901" or i.tag == "949"]
+    elif all(isinstance(i, dict) for i in fields) and all("tag" in i for i in fields):
+        fields = [i for i in fields if i["tag"] == "901" or i["tag"] == "949"]
+    else:
+        fields = [i for i in fields if "901" in i or "949" in i]
+    subfields: list = []
+    for field in fields:
+        if isinstance(field, MarcField):
+            tag = field.tag
+        elif isinstance(field, dict) and "tag" in field:
+            tag = field["tag"]
+        else:
+            tag = list(field.keys())[0]
+        code = "a" if tag == "901" else "v"
+        subfields.extend(get_subfield_from_code(field, tag=tag, code=code))
+    deduped_subfields = list(set(subfields))
+    if len(deduped_subfields) == 1:
+        return deduped_subfields[0]
+    else:
+        return None
+
+
+def pattern_match(subfield_list: list, pattern: str) -> bool:
+    match pattern:
+        case "multivol":
+            regex = re.compile(r"^(\d+)( v\.| volumes)( :$| ;$|$| $)")
+        case "pamphlet":
+            regex = re.compile(r"^(\d|[0-4][0-9])( p\.| pages)( :$| ;$|$| $)")
+        case "catalogues":
+            regex = re.compile(r"^[cC]atalogue(s?) [rR]aisonn[eé](s?)")
+        case "aux_call_no":
+            regex = re.compile(r"^ReCAP 2[345]-$")
+        case _:
+            return False
+    return any(i is not None for i in [regex.match(str(i)) for i in subfield_list])
+
+
+def tag_discriminator(field: Union[MarcField, dict]) -> str:
+    tag = field.tag if isinstance(field, MarcField) else list(field.keys())[0]
+    if tag in [i.value for i in AllFields]:
+        return tag
+    else:
+        return "data_field"
+
+
+AuxOtherFields = Annotated[
+    Union[
+        Annotated[ControlField001, Tag("001")],
+        Annotated[ControlField003, Tag("003")],
+        Annotated[ControlField005, Tag("005")],
+        Annotated[ControlField006, Tag("006")],
+        Annotated[ControlField007, Tag("007")],
+        Annotated[ControlField008, Tag("008")],
+        Annotated[LCClass, Tag("050")],
+        Annotated[AuxBibCallNo, Tag("852")],
+        Annotated[BibVendorCode, Tag("901")],
+        Annotated[LibraryField, Tag("910")],
+        Annotated[OtherDataField, Tag("949")],
+        Annotated[OrderField, Tag("960")],
+        Annotated[InvoiceField, Tag("980")],
+        Annotated[OtherDataField, Tag("data_field")],
+    ],
+    Discriminator(tag_discriminator),
+]
+
+MonographFields = Annotated[
+    Union[
+        Annotated[ControlField001, Tag("001")],
+        Annotated[ControlField003, Tag("003")],
+        Annotated[ControlField005, Tag("005")],
+        Annotated[ControlField006, Tag("006")],
+        Annotated[ControlField007, Tag("007")],
+        Annotated[ControlField008, Tag("008")],
+        Annotated[LCClass, Tag("050")],
+        Annotated[BibCallNo, Tag("852")],
+        Annotated[BibVendorCode, Tag("901")],
+        Annotated[LibraryField, Tag("910")],
+        Annotated[ItemField, Tag("949")],
+        Annotated[OrderField, Tag("960")],
+        Annotated[InvoiceField, Tag("980")],
+        Annotated[MonographDataField, Tag("data_field")],
+    ],
+    Discriminator(tag_discriminator),
+]
+
+OtherFields = Annotated[
+    Union[
+        Annotated[ControlField001, Tag("001")],
+        Annotated[ControlField003, Tag("003")],
+        Annotated[ControlField005, Tag("005")],
+        Annotated[ControlField006, Tag("006")],
+        Annotated[ControlField007, Tag("007")],
+        Annotated[ControlField008, Tag("008")],
+        Annotated[LCClass, Tag("050")],
+        Annotated[OtherDataField, Tag("852")],
+        Annotated[BibVendorCode, Tag("901")],
+        Annotated[LibraryField, Tag("910")],
+        Annotated[OtherDataField, Tag("949")],
+        Annotated[OrderField, Tag("960")],
+        Annotated[InvoiceField, Tag("980")],
+        Annotated[OtherDataField, Tag("data_field")],
+    ],
+    Discriminator(tag_discriminator),
+]
--- tests/test_adapters.py
+++ tests/test_adapters.py
@@ -1,34 +1,346 @@
+from typing import get_args
+from pydantic import TypeAdapter, Discriminator, Tag
+from pymarc import Field as MarcField
 import pytest
-from record_validator.adapters import (
-    MonographRecordAdapter,
-    OtherRecordAdapter,
-    FieldAdapter,
-)
 from record_validator.adapters import (
     get_material_type,
     get_monograph_tag,
     get_other_tag,
+    MonographRecordAdapter,
+    OtherRecordAdapter,
+    FieldAdapter,
+    get_adapter,
+    get_record_type,
+    get_vendor_code,
+    pattern_match,
+    tag_discriminator,
+    AuxOtherFields,
+    MonographFields,
+    OtherFields,
+)
+from record_validator.field_models import (
+    AuxBibCallNo,
+    MonographDataField,
+    OtherDataField,
 )
 
 
+def test_get_adapter(stub_record):
+    record_type, adapter = get_adapter(stub_record.fields)
+    assert isinstance(record_type, str)
+    assert isinstance(adapter, TypeAdapter)
+
+
+def test_get_adapter_monograph_evp(stub_record):
+    record_type, adapter = get_adapter(stub_record.fields)
+    schema = adapter.json_schema(by_alias=True)
+    assert record_type == "evp_monograph"
+    assert isinstance(adapter, TypeAdapter)
+    assert sorted([i for i in schema["$defs"].keys()]) == [
+        "BibCallNo",
+        "BibVendorCode",
+        "ControlField001",
+        "ControlField003",
+        "ControlField005",
+        "ControlField006",
+        "ControlField007",
+        "ControlField008",
+        "InvoiceField",
+        "ItemField",
+        "LCClass",
+        "LibraryField",
+        "MonographDataField",
+        "OrderField",
+    ]
+
+
+def test_get_adapter_monograph_no_vendor(stub_record):
+    stub_record.remove_fields("901")
+    stub_record["949"].delete_subfield("v")
+    record_type, adapter = get_adapter(stub_record.fields)
+    schema = adapter.json_schema(by_alias=True)
+    assert record_type == "monograph"
+    assert isinstance(adapter, TypeAdapter)
+    assert sorted([i for i in schema["$defs"].keys()]) == [
+        "BibCallNo",
+        "BibVendorCode",
+        "ControlField001",
+        "ControlField003",
+        "ControlField005",
+        "ControlField006",
+        "ControlField007",
+        "ControlField008",
+        "InvoiceField",
+        "ItemField",
+        "LCClass",
+        "LibraryField",
+        "MonographDataField",
+        "OrderField",
+    ]
+
+
+def test_get_adapter_other_auxam(stub_aux_other_record):
+    record_type, adapter = get_adapter(stub_aux_other_record.fields)
+    schema = adapter.json_schema(by_alias=True)
+    assert record_type == "auxam_other"
+    assert isinstance(adapter, TypeAdapter)
+    assert sorted([i for i in schema["$defs"].keys()]) == [
+        "AuxBibCallNo",
+        "BibVendorCode",
+        "ControlField001",
+        "ControlField003",
+        "ControlField005",
+        "ControlField006",
+        "ControlField007",
+        "ControlField008",
+        "InvoiceField",
+        "LCClass",
+        "LibraryField",
+        "OrderField",
+        "OtherDataField",
+    ]
+
+
+def test_get_adapter_other_evp(stub_pamphlet_record):
+    record_type, adapter = get_adapter(stub_pamphlet_record.fields)
+    schema = adapter.json_schema(by_alias=True)
+    assert record_type == "evp_other"
+    assert isinstance(adapter, TypeAdapter)
+    assert sorted([i for i in schema["$defs"].keys()]) == [
+        "BibVendorCode",
+        "ControlField001",
+        "ControlField003",
+        "ControlField005",
+        "ControlField006",
+        "ControlField007",
+        "ControlField008",
+        "InvoiceField",
+        "LCClass",
+        "LibraryField",
+        "OrderField",
+        "OtherDataField",
+    ]
+
+
+def test_get_adapter_other_no_vendor(stub_pamphlet_record):
+    stub_pamphlet_record.remove_fields("901")
+    record_type, adapter = get_adapter(stub_pamphlet_record.fields)
+    schema = adapter.json_schema(by_alias=True)
+    assert record_type == "other"
+    assert isinstance(adapter, TypeAdapter)
+    assert sorted([i for i in schema["$defs"].keys()]) == [
+        "BibVendorCode",
+        "ControlField001",
+        "ControlField003",
+        "ControlField005",
+        "ControlField006",
+        "ControlField007",
+        "ControlField008",
+        "InvoiceField",
+        "LCClass",
+        "LibraryField",
+        "OrderField",
+        "OtherDataField",
+    ]
+
+
+def test_get_record_type_monograph(stub_record):
+    assert get_record_type(fields=stub_record.fields, vendor_code="EVP") == "monograph"
+
+
+def test_get_record_type_dance(stub_dance_record):
+    assert (
+        get_record_type(fields=stub_dance_record.fields, vendor_code="EVP") == "other"
+    )
+
+
+def test_get_record_type_pamphlet(stub_pamphlet_record):
+    assert (
+        get_record_type(fields=stub_pamphlet_record.fields, vendor_code="EVP")
+        == "other"
+    )
+
+
+def test_get_record_type_catalogue(stub_catalogue_record):
+    assert (
+        get_record_type(fields=stub_catalogue_record.fields, vendor_code="EVP")
+        == "other"
+    )
+
+
+def test_get_record_type_multivol(stub_multivol_record):
+    assert (
+        get_record_type(fields=stub_multivol_record.fields, vendor_code="EVP")
+        == "other"
+    )
+
+
+def test_get_record_type_auxam_other(stub_aux_other_record):
+    assert (
+        get_record_type(fields=stub_aux_other_record.fields, vendor_code="AUXAM")
+        == "other"
+    )
+
+
+def test_get_record_type_bad_input():
+    assert get_record_type(fields=["foo"], vendor_code="AUXAM") == "other"
+
+
+def test_get_record_type_dict(stub_record):
+    record_dict = stub_record.as_dict()
+    assert (
+        get_record_type(fields=record_dict["fields"], vendor_code="EVP") == "monograph"
+    )
+
+
+def test_get_vendor_code_evp(stub_record):
+    assert get_vendor_code(fields=stub_record.fields) == "EVP"
+
+
+def test_get_vendor_code_auxam(stub_aux_other_record):
+    assert get_vendor_code(fields=stub_aux_other_record.fields) == "AUXAM"
+
+
+def test_get_vendor_code_leila(stub_leila_monograph):
+    assert get_vendor_code(fields=stub_leila_monograph.fields) == "LEILA"
+
+
+def test_get_vendor_code_none():
+    assert get_vendor_code(fields=["foo"]) is None
+
+
+def test_get_vendor_code_bad_input(stub_record):
+    stub_record["901"].add_subfield("a", "foo")
+    assert get_vendor_code(fields=stub_record.fields) is None
+
+
+def test_get_vendor_code_dict():
+    fields = [
+        {"tag": "001", "value": "on1381158740"},
+        {
+            "tag": "901",
+            "ind1": " ",
+            "ind2": " ",
+            "subfields": [
+                {"a": "EVP"},
+            ],
+        },
+    ]
+    assert get_vendor_code(fields=fields) == "EVP"
+
+
+def test_get_vendor_code_pymarc_dict(stub_record):
+    stub_record_dict = stub_record.as_dict()
+    assert get_vendor_code(fields=stub_record_dict["fields"]) == "EVP"
+
+
+@pytest.mark.parametrize(
+    "subfields, pattern, expected",
+    [
+        (["2 volumes :"], "multivol", True),
+        (["2 volumes :"], "pamphlet", False),
+        (["5 pages :", "illustrations"], "pamphlet", True),
+        (["5 pages :", "illustrations"], "catalogues", False),
+        (["Catalogues Raisonnes"], "catalogues", True),
+        (["Catalogues Raisonnés"], "catalogues", True),
+        (["Catalogue Raisonnés"], "catalogues", True),
+        (["foo"], "catalogues", False),
+        (["ReCAP 24-"], "aux_call_no", True),
+        (["ReCAP 25-"], "aux_call_no", True),
+        (["ReCAP 24-100000"], "aux_call_no", False),
+        (["foo"], "aux_call_no", False),
+        (None, "foo", False),
+    ],
+)
+def test_pattern_match(subfields, pattern, expected):
+    assert pattern_match(subfields, pattern) == expected
+
+
+@pytest.mark.parametrize(
+    "tag, expected",
+    [
+        ("245", "data_field"),
+        ("300", "data_field"),
+        ("960", "960"),
+        ("949", "949"),
+        ("852", "852"),
+        ("008", "008"),
+        ("001", "001"),
+    ],
+)
+def test_tag_discriminator_from_dict(tag, expected):
+    field = {tag: {"ind1": " ", "ind2": " ", "subfields": [{"a": "foo"}]}}
+    assert tag_discriminator(field) == expected
+
+
+@pytest.mark.parametrize(
+    "tag, expected",
+    [
+        ("245", "data_field"),
+        ("300", "data_field"),
+        ("960", "960"),
+        ("949", "949"),
+        ("852", "852"),
+        ("008", "008"),
+        ("001", "001"),
+    ],
+)
+def test_tag_discriminator_from_marc(stub_record, tag, expected):
+    field = stub_record.get(tag)
+    assert isinstance(field, MarcField)
+    assert field.tag == tag
+    assert tag_discriminator(field) == expected
+
+
+def test_AuxOtherFields():
+    aux_other_args = get_args(AuxOtherFields)
+    assert len(aux_other_args) == 2
+    assert aux_other_args[1] == Discriminator(tag_discriminator)
+    union_args = [get_args(i) for i in get_args(aux_other_args[0])]
+    assert len(union_args) == 14
+    assert (AuxBibCallNo, Tag("852")) in union_args
+    assert (OtherDataField, Tag("data_field")) in union_args
+    assert (OtherDataField, Tag("949")) in union_args
+
+
+def test_MonographFields():
+    monograph_args = get_args(MonographFields)
+    assert len(monograph_args) == 2
+    assert monograph_args[1] == Discriminator(tag_discriminator)
+    union_args = [get_args(i) for i in get_args(monograph_args[0])]
+    assert len(union_args) == 14
+    assert (MonographDataField, Tag("data_field")) in union_args
+
+
+def test_OtherFields():
+    other_args = get_args(OtherFields)
+    assert len(other_args) == 2
+    assert other_args[1] == Discriminator(tag_discriminator)
+    union_args = [get_args(i) for i in get_args(other_args[0])]
+    assert len(union_args) == 14
+    assert (OtherDataField, Tag("data_field")) in union_args
+    assert (OtherDataField, Tag("852")) in union_args
+    assert (OtherDataField, Tag("949")) in union_args
+
+
 def test_get_material_type_monograph(stub_record):
-    assert get_material_type(stub_record.fields) == "monograph"
+    assert get_material_type(fields=stub_record.fields) == "monograph"
 
 
 def test_get_material_type_dance(stub_dance_record):
-    assert get_material_type(stub_dance_record.fields) == "other"
+    assert get_material_type(fields=stub_dance_record.fields) == "other"
 
 
 def test_get_material_type_pamphlet(stub_pamphlet_record):
-    assert get_material_type(stub_pamphlet_record.fields) == "other"
+    assert get_material_type(fields=stub_pamphlet_record.fields) == "other"
 
 
 def test_get_material_type_catalogue(stub_catalogue_record):
-    assert get_material_type(stub_catalogue_record.fields) == "other"
+    assert get_material_type(fields=stub_catalogue_record.fields) == "other"
 
 
 def test_get_material_type_multivol(stub_multivol_record):
-    assert get_material_type(stub_multivol_record.fields) == "other"
+    assert get_material_type(fields=stub_multivol_record.fields) == "other"
 
 
 def test_get_material_type_dict(stub_record):
@@ -104,6 +416,7 @@ def test_FieldAdapter():
     schema = FieldAdapter.json_schema(by_alias=True)
     assert sorted([i for i in schema["$defs"].keys()]) == sorted(
         [
+            "AuxBibCallNo",
             "BibCallNo",
             "BibVendorCode",
             "ControlField001",
@@ -116,7 +429,7 @@ def test_FieldAdapter():
             "ItemField",
             "LCClass",
             "LibraryField",
-            "MonographOtherField",
+            "MonographDataField",
             "OrderField",
             "OtherDataField",
         ]
@@ -139,7 +452,7 @@ def test_MonographRecordAdapter():
             "ItemField",
             "LCClass",
             "LibraryField",
-            "MonographOtherField",
+            "MonographDataField",
             "OrderField",
         ]
     )
