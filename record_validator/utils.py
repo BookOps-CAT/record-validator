16af32d858682b46982c7391f70db46008ce7229
Charlotte Kostelic <charlottekostelic@gmail.com>
2024-10-04 11:31:43 -0400

    refactored functions in utils.py

--- record_validator/utils.py
+++ record_validator/utils.py
@@ -1,48 +1,80 @@
-from typing import Union
+from itertools import chain
+import re
+from typing import Any, Dict, List, Union
 from pymarc import Field as MarcField
 
 
-def get_subfield_from_code(
-    field: Union[MarcField, dict],
-    tag: str,
-    code: str,
-) -> Union[list[str], list[None]]:
-    if not isinstance(field, (MarcField, dict)):
-        return None
+def dict2subfield(field: Dict[str, Any], code: str) -> List[Union[str, None]]:
+    subfields = field[next(iter(field))]["subfields"]
+    subfield_codes = list(chain(*[i.keys() for i in subfields]))
+    out_list = []
+    if code in subfield_codes:
+        out_list.extend([i[code] for i in subfields if code in i.keys()])
+    else:
+        out_list.append(None)
+    return out_list
+
+
+def field2dict(field: Union[MarcField, Dict[str, Any]]) -> Dict[str, Any]:
+    if isinstance(field, MarcField) and field.tag.startswith("00"):
+        return {field.tag: field.data}
+    elif isinstance(field, dict) and "tag" in field and "subfields" not in field:
+        return {field["tag"]: field["value"]}
+    elif isinstance(field, dict) and all(isinstance(i, str) for i in field.values()):
+        return {tag: value for tag, value in field.items()}
     elif isinstance(field, MarcField):
-        subfields = [i[1] for i in field.subfields if code == i[0]]
+        tag, ind1, ind2 = field.tag, field.indicator1, field.indicator2
+        subfields = [{i[0]: i[1]} for i in field.subfields]
+    elif isinstance(field, dict) and "tag" in field and "subfields" in field:
+        tag, ind1, ind2 = field["tag"], field["ind1"], field["ind2"]
+        subfields = field["subfields"]
     else:
-        all_subfields = (
-            field.get("subfields") if "tag" in field else field[tag].get("subfields")
-        )
-        subfields = [i[code] for i in all_subfields if code in i.keys()]
-    if subfields == []:
-        return [None]
-    else:
-        return subfields
+        ((tag, data),) = field.items()
+        ind1, ind2, subfields = data["ind1"], data["ind2"], data["subfields"]
+    return {tag: {"ind1": ind1, "ind2": ind2, "subfields": subfields}}
 
 
-def get_vendor_code(fields: list) -> Union[str, None]:
-    if not all(isinstance(i, (MarcField, dict)) for i in fields):
-        return None
-    if all(isinstance(i, MarcField) for i in fields):
-        fields = [i for i in fields if i.tag == "901" or i.tag == "949"]
-    elif all(isinstance(i, dict) for i in fields) and all("tag" in i for i in fields):
-        fields = [i for i in fields if i["tag"] == "901" or i["tag"] == "949"]
+def get_record_type(fields: List[Union[MarcField, Dict[str, Any]]]) -> str:
+    if not isinstance(fields, list) or not all(
+        isinstance(i, (MarcField, dict)) for i in fields
+    ):
+        return "other"
+    field_list = [field2dict(i) for i in fields]
+
+    item_vendor = [dict2subfield(i, "v") for i in field_list if "949" in i]
+    bib_vendor = [dict2subfield(i, "a") for i in field_list if "901" in i]
+    deduped_vendor = list(set(chain(*item_vendor + bib_vendor)))
+    if len(deduped_vendor) == 1 and isinstance(deduped_vendor[0], str):
+        vendor_code = deduped_vendor[0].lower()
     else:
-        fields = [i for i in fields if "901" in i or "949" in i]
-    subfields: list = []
-    for field in fields:
-        if isinstance(field, MarcField):
-            tag = field.tag
-        elif isinstance(field, dict) and "tag" in field:
-            tag = field["tag"]
-        else:
-            tag = list(field.keys())[0]
-        code = "a" if tag == "901" else "v"
-        subfields.extend(get_subfield_from_code(field, tag=tag, code=code))
-    deduped_subfields = list(set(subfields))
-    if len(deduped_subfields) == 1:
-        return deduped_subfields[0]
+        vendor_code = None
+    aux_call_no_fields = [
+        i for i in field_list if "852" in i and vendor_code == "auxam"
+    ]
+    subject_fields = [i for i in field_list if next(iter(i)).startswith("6")]
+
+    aux_call_nos = list(chain(*[dict2subfield(i, "h") for i in aux_call_no_fields]))
+    order_locs = list(chain(*[dict2subfield(i, "t") for i in field_list if "960" in i]))
+    phys_desc = list(chain(*[dict2subfield(i, "a") for i in field_list if "300" in i]))
+    subjects = list(chain(*[dict2subfield(i, "v") for i in subject_fields]))
+
+    aux_pattern = re.compile(r"^ReCAP 2[345]-$")
+    catalogue = re.compile(r"^[cC]atalogue(s?) [rR]aisonn[eé](s?)")
+    multivol = re.compile(r"^(\d+)( v\.| volumes)( :$| ;$|$| $)")
+    pamphlet = re.compile(r"^(\d|[0-4][0-9])( p\.| pages)( :$| ;$|$| $)")
+    if any(j is not None for j in [aux_pattern.match(str(i)) for i in aux_call_nos]):
+        material_type = "other"
+    elif any(j is not None for j in [catalogue.match(str(i)) for i in subjects]):
+        material_type = "other"
+    elif any(i == "PAD" for i in order_locs) is True:
+        material_type = "other"
+    elif any(j is not None for j in [multivol.match(str(i)) for i in phys_desc]):
+        material_type = "other"
+    elif any(j is not None for j in [pamphlet.match(str(i)) for i in phys_desc]):
+        material_type = "other"
     else:
-        return None
+        material_type = "monograph"
+    if vendor_code is not None:
+        return f"{vendor_code}_{material_type}"
+    else:
+        return material_type
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,85 +1,85 @@
 import pytest
-from record_validator.utils import get_subfield_from_code, get_vendor_code
+from record_validator.utils import (
+    dict2subfield,
+    field2dict,
+    get_record_type,
+)
 
 
 @pytest.mark.parametrize(
     "tag, code, expected",
-    [("960", "t", ["MAF"]), ("050", "a", ["F00"])],
+    [
+        ("050", "a", "F00"),
+        ("245", "a", "Title :"),
+        ("300", "a", "100 pages :"),
+        ("901", "a", "EVP"),
+        ("949", "v", "EVP"),
+    ],
 )
-def test_get_subfield_from_code_marc(stub_record, tag, code, expected):
-    field = stub_record.get(tag)
-    assert get_subfield_from_code(field=field, tag=tag, code=code) == expected
+def test_dict2subfield(stub_record, tag, code, expected):
+    record_dict = stub_record.as_dict()
+    field = [i for i in record_dict["fields"] if tag in i.keys()][0]
+    assert dict2subfield(field, code) == [expected]
 
 
-def test_get_subfield_from_code_dict():
-    field = {
-        "tag": "980",
-        "ind1": " ",
-        "ind2": " ",
-        "subfields": [{"a": "foo"}, {"b": "bar"}, {"c": "baz"}],
-    }
-    assert get_subfield_from_code(field=field, tag="980", code="a") == ["foo"]
-    assert get_subfield_from_code(field=field, tag="980", code="b") == ["bar"]
-    assert get_subfield_from_code(field=field, tag="980", code="c") == ["baz"]
+def test_dict2subfield_test():
+    field = {"852": {"ind1": "8", "ind2": " ", "subfields": [{"h": "ReCAP 24-100000"}]}}
+    assert dict2subfield(field, "h") == ["ReCAP 24-100000"]
 
 
-def test_get_subfield_from_code_pymarc_dict(stub_record):
+def test_field2dict_marc(stub_record):
+    fields = stub_record.fields
+    assert len([field2dict(i) for i in fields]) == len(fields)
+    assert field2dict(fields[0]) == {"001": "on1381158740"}
+
+
+def test_field2dict_pymarc_dict(stub_record):
     stub_record_dict = stub_record.as_dict()
-    field = stub_record_dict["fields"][-1]
-    assert get_subfield_from_code(field=field, tag="980", code="a") == ["240101"]
+    fields = stub_record_dict["fields"]
+    assert len([field2dict(i) for i in fields]) == len(fields)
+    assert field2dict(fields[0]) == {"001": "on1381158740"}
 
 
-def test_get_subfield_from_code_None():
-    field = {
-        "tag": "980",
-        "ind1": " ",
-        "ind2": " ",
-        "subfields": [{"a": "foo"}, {"b": "bar"}, {"c": "baz"}],
-    }
-    assert get_subfield_from_code(field=field, tag="980", code="z") == [None]
-
-
-def test_get_subfield_from_code_other():
-    field = ("980", " ", " ", [("a", "foo"), ("b", "bar"), ("c", "baz")])
-    assert get_subfield_from_code(field=field, tag="980", code="a") is None
-
-
-def test_get_vendor_code_evp(stub_record):
-    assert get_vendor_code(fields=stub_record.fields) == "EVP"
-
-
-def test_get_vendor_code_auxam(stub_aux_other_record):
-    assert get_vendor_code(fields=stub_aux_other_record.fields) == "AUXAM"
-
-
-def test_get_vendor_code_leila(stub_leila_monograph):
-    assert get_vendor_code(fields=stub_leila_monograph.fields) == "LEILA"
-
-
-def test_get_vendor_code_none():
-    assert get_vendor_code(fields=["foo"]) is None
-
-
-def test_get_vendor_code_bad_input(stub_record):
-    stub_record["901"].add_subfield("a", "foo")
-    assert get_vendor_code(fields=stub_record.fields) is None
-
-
-def test_get_vendor_code_dict():
+def test_field2dict_dict():
     fields = [
         {"tag": "001", "value": "on1381158740"},
-        {
-            "tag": "901",
-            "ind1": " ",
-            "ind2": " ",
-            "subfields": [
-                {"a": "EVP"},
-            ],
-        },
+        {"tag": "300", "ind1": " ", "ind2": " ", "subfields": [{"a": "100 pages :"}]},
     ]
-    assert get_vendor_code(fields=fields) == "EVP"
+    assert len([field2dict(i) for i in fields]) == len(fields)
+    assert field2dict(fields[0]) == {"001": "on1381158740"}
+    assert field2dict(fields[1]) == {
+        "300": {"ind1": " ", "ind2": " ", "subfields": [{"a": "100 pages :"}]}
+    }
 
 
-def test_get_vendor_code_pymarc_dict(stub_record):
-    stub_record_dict = stub_record.as_dict()
-    assert get_vendor_code(fields=stub_record_dict["fields"]) == "EVP"
+def test_get_record_type_monograph(stub_leila_monograph):
+    assert get_record_type(fields=stub_leila_monograph.fields) == "leila_monograph"
+
+
+def test_get_record_type_dance(stub_dance_record):
+    assert get_record_type(fields=stub_dance_record.fields) == "evp_other"
+
+
+def test_get_record_type_pamphlet(stub_pamphlet_record):
+    assert get_record_type(fields=stub_pamphlet_record.fields) == "evp_other"
+
+
+def test_get_record_type_catalogue(stub_catalogue_record):
+    assert get_record_type(fields=stub_catalogue_record.fields) == "evp_other"
+
+
+def test_get_record_type_multivol(stub_multivol_record):
+    assert get_record_type(fields=stub_multivol_record.fields) == "evp_other"
+
+
+def test_get_record_type_auxam_other(stub_aux_other_record):
+    assert get_record_type(fields=stub_aux_other_record.fields) == "auxam_other"
+
+
+def test_get_record_type_bad_input():
+    assert get_record_type(fields=["foo"]) == "other"
+
+
+def test_get_record_type_dict(stub_record):
+    record_dict = stub_record.as_dict()
+    assert get_record_type(fields=record_dict["fields"]) == "evp_monograph"
